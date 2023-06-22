12e4d6babdaf2d16297a07db5e03972649f046f7
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-06-22 19:44:14 +0100

    chore: works

--- src/storage.rs
+++ src/storage.rs
@@ -19,7 +19,8 @@ pub struct Storage<D: Device> {
 impl<D: Device> Storage<D> {
     ///Create a new Storage on the given device.
     pub fn to<Ext: Device>(&self, ext: Ext) -> Result<Storage<Ext>, anyhow::Error> {
-        let mut dst = ext.allocate(self.layout, AllocMode::COPY_WRITE)?;
+        //Alloc mode here needs to be
+        let mut dst = ext.allocate(self.layout, AllocMode::ALL)?;
         self.device.copy_to(&self.data, &mut dst, &ext)?;
 
         Ok(Storage {
